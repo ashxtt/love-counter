b9339bb50bcc90ce77c2bbe0149433ef22c6e8c5
Ashton Bowens <ashxgrey11@gmail.com>
2022-12-03 14:44:51 -0800

    ready to deploy

--- src/components/loveClick.js
+++ src/components/loveClick.js
@@ -29,7 +29,7 @@ function Counter() {
              <button  className="reset" onClick={handleReset}><i class="fa fa-refresh"></i></button>
             </section>
             <div className="gif">
-                <img src="https://i.pinimg.com/originals/d3/d6/f7/d3d6f72695eed14ba7a8b5a8169eae78.gif"/>
+                <img src="https://i.pinimg.com/originals/d3/d6/f7/d3d6f72695eed14ba7a8b5a8169eae78.gif" alt="bears-love-gif"/>
             </div>
         
         </div>
